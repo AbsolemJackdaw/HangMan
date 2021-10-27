054f360bc12a690cfc21e7891146cbc51b2e04fe
Subaraki <robbeenaxel@hotmail.com>
2021-10-27 22:00:15 +0200

    Fix Overlay Remove Gui overlay when hanging

---
+++ src/main/java/subaraki/hangman/events/DisableGuiOnHang.java
@@ -0,0 +1,22 @@
+package subaraki.hangman.events;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import subaraki.hangman.entity.NooseEntity;
+import subaraki.hangman.mod.HangMan;
+
+@Mod.EventBusSubscriber(modid = HangMan.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class DisableGuiOnHang {
+
+    @SubscribeEvent
+    public static void gameoverlayEvent(RenderGameOverlayEvent.Pre event) {
+        if (event.getType().equals(RenderGameOverlayEvent.ElementType.ALL)) {
+            if (Minecraft.getInstance().player.getVehicle() instanceof NooseEntity noose) {
+                event.setCanceled(true);
+            }
+        }
+    }
+}
