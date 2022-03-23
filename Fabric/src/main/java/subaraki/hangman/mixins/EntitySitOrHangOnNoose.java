53e01bdea4edd01839b555f329a88cf6eed80444
Subaraki <robbeenaxel@hotmail.com>
2022-03-23 17:02:18 +0100

    Remove unused code

--- Fabric/src/main/java/subaraki/hangman/mixins/EntitySitOrHangOnNoose.java
+++ Fabric/src/main/java/subaraki/hangman/mixins/EntitySitOrHangOnNoose.java
@@ -10,15 +10,6 @@ import subaraki.hangman.entity.NooseEntity;
 @Mixin(LivingEntityRenderer.class)
 public abstract class EntitySitOrHangOnNoose {
 
-//    @Inject(method = "isPassenger", at = @At("RETURN"), cancellable = true)
-//    public void isPassenger(CallbackInfoReturnable<Boolean> cir) {
-//        boolean flag = cir.getReturnValue();
-//        boolean flag2 = true;
-//        if (((EntitySitMixing) (Object) this).getVehicle() instanceof NooseEntity noose) // (object)this will change to Enity on runtime. mixin magic !
-//            flag2 = noose.shouldHangedEntitySit();
-//        cir.setReturnValue(flag);
-//    }
-
     @Redirect(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isPassenger()Z"))
     public boolean renderEdit(LivingEntity instance) {
         boolean flag = true;
