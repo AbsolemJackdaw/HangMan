7932941f698098fdf6dc13e828e6a9442577429d
Axel Cornelis PXL <axel.Cornelis@student.pxl.be>
2026-01-12 10:58:11 +0100

    fix player render on noose mixin

--- fabric/src/main/java/subaraki/hangman/mixins/PatchLevelRenderCameraRendering.java
+++ fabric/src/main/java/subaraki/hangman/mixins/PatchLevelRenderCameraRendering.java
@@ -1,39 +1,27 @@
 package subaraki.hangman.mixins;
 
-import com.mojang.blaze3d.buffers.GpuBufferSlice;
-import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Camera;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
-import org.joml.Matrix4f;
-import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import subaraki.hangman.entity.CameraPlayerOnNoose;
 import subaraki.hangman.entity.NooseEntity;
-
-import java.util.List;
 
 @Mixin(EntityRenderDispatcher.class)
 public class PatchLevelRenderCameraRendering {
 
     @Inject(method = "shouldRender",
             at = @At("HEAD"))
-    private void applyPatch(E entity, Frustum frustum, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
+    private <E extends Entity> void applyPatch(E entity, Frustum frustum, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
         Minecraft minecraft = Minecraft.getInstance();
         LocalPlayer player = minecraft.player;
 
         if (player != null && player.getVehicle() instanceof NooseEntity && minecraft.getCameraEntity() instanceof CameraPlayerOnNoose)
-            entities.add(player);
+            cir.setReturnValue(true);
     }
 }
