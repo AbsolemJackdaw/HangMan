59e8a9f3df88c87b5de9556d9962080050c7e158
Axel Cornelis PXL <axel.Cornelis@student.pxl.be>
2026-01-13 13:55:23 +0100

    fix player render on noose mixin update to 1.21.11

--- fabric/src/main/java/subaraki/hangman/mixins/PatchLevelRenderCameraRendering.java
+++ fabric/src/main/java/subaraki/hangman/mixins/PatchLevelRenderCameraRendering.java
@@ -1,27 +1,32 @@
 package subaraki.hangman.mixins;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.culling.Frustum;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import subaraki.hangman.entity.CameraPlayerOnNoose;
-import subaraki.hangman.entity.NooseEntity;
 
-@Mixin(EntityRenderDispatcher.class)
-public class PatchLevelRenderCameraRendering {
+@Mixin(LevelRenderer.class)
+public abstract class PatchLevelRenderCameraRendering {
 
-    @Inject(method = "shouldRender",
-            at = @At("HEAD"))
-    private <E extends Entity> void applyPatch(E entity, Frustum frustum, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
-        Minecraft minecraft = Minecraft.getInstance();
-        LocalPlayer player = minecraft.player;
+    @Shadow
+    @Final
+    private Minecraft minecraft;
 
-        if (player != null && player.getVehicle() instanceof NooseEntity && minecraft.getCameraEntity() instanceof CameraPlayerOnNoose)
-            cir.setReturnValue(true);
+    @Definition(id = "LocalPlayer", type = LocalPlayer.class)
+    @Expression("? instanceof LocalPlayer")
+    @WrapOperation(method = "extractVisibleEntities", at = @At("MIXINEXTRAS:EXPRESSION"))
+    public boolean help(Object object, Operation<Boolean> original, @Local Camera camera, @Local Entity entity) {
+        var flag = original.call(object) && (object != minecraft.player || minecraft.player.isSpectator());
+        return flag;
     }
 }
